968aaf0c6f213fc4e6cde8910d21796713f1453e
moralesmeza <moralesmeza28@gmail.com>
2024-04-09 20:41:16 -0500

    upd

--- appengine/standar-frontend/main.py
+++ appengine/standar-frontend/main.py
@@ -3,9 +3,17 @@
 import logging
 
 # [START imports]
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request ,jsonify
+import os
 
 # [END imports]
+
+server_url = os.environ.get('SERVER_URL')
+
+if server_url is None :
+   server_url = 'http://127.0.0.1:8090'
+
+
 
 # [START create_app]
 app = Flask(__name__)
