d5fc5c29fa63e6f17115672dd5ac70a1b3e87256
moralesmeza <moralesmeza28@gmail.com>
2024-04-09 19:51:06 -0500

    Actualización

--- appengine/standar-frontend/main.py
+++ appengine/standar-frontend/main.py
@@ -1,34 +1,47 @@
 
 # [START app]
+import logging
 
 # [START imports]
-from flask import Flask, render_template, request ,jsonify
-import os
+from flask import Flask, render_template, request
 
 # [END imports]
 
-server_url = os.environ.get('SERVER_URL')
-
-if server_url is None :
-   server_url = 'http://127.0.0.1:8090'
-
-
 # [START create_app]
 app = Flask(__name__)
-
 # [END create_app]
 
 
 # [START form]
 @app.route("/")
 def form():
-    return render_template("form.html",server_url=server_url)
+    return render_template("form.html")
 
 
 # [END form]
 
 
+# [START submitted]
+@app.route("/submitted", methods=["POST"])
+def submitted_form():
+    name = request.form["name"]
+    email = request.form["email"]
+    document = request.form["document"]
+    comments = request.form["comments"]
 
+    # [END submitted]
+    # [START render_template]
+    return render_template(
+        "submitted_form.html", name=name, email=email, document=document, comments=comments
+    )
+    # [END render_template]
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception("An error occurred during a request.")
+    return "An internal error occurred.", 500
 
 
 # [END app]
