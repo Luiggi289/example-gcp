3d7978e63cddccf37a9816932e526d6c59671acd
moralesmeza <moralesmeza28@gmail.com>
2024-04-26 13:28:15 -0500

    upd

--- dataproc/demo/segment.py
+++ dataproc/demo/segment.py
@@ -25,50 +25,12 @@ df = spark.read.format('bigquery') \
 
 print(df.columns)
 
-
-# Calcular las ventas totales por cliente y fecha
-ventas_por_cliente = df.groupBy("user_id", "order_date") \
-    .agg({"sale_price": "sum"}) \
-    .withColumnRenamed("sum(sale_price)", "total_venta")
-
-# Calcular la diferencia en días y el cambio en las ventas por cliente
-tendencia_ventas = ventas_por_cliente.groupBy("user_id") \
-    .agg(datediff(spark_max("order_date"), spark_min("order_date")).alias("dias_compra"),
-         (spark_max("total_venta") - spark_min("total_venta")).alias("cambio_venta"))
-
-# Calcular los percentiles para la segmentación
-percentiles = tendencia_ventas.selectExpr("percentile_approx(dias_compra, 0.33)", 
-                                          "percentile_approx(dias_compra, 0.66)",
-                                          "percentile_approx(cambio_venta, 0.33)",
-                                          "percentile_approx(cambio_venta, 0.66)") \
-                               .collect()[0]
-
-corto, largo, negativo, neutro = percentiles
-
-# Definir expresiones para las condiciones de segmentación
-segmento_expr = expr("""
-    CASE 
-        WHEN dias_compra <= {} AND cambio_venta <= {} THEN 'Bajo y Disminuyendo'
-        WHEN dias_compra <= {} AND cambio_venta > {} AND cambio_venta <= {} THEN 'Bajo y Estable'
-        WHEN dias_compra <= {} AND cambio_venta > {} THEN 'Bajo y Aumentando'
-        WHEN dias_compra > {} AND dias_compra <= {} AND cambio_venta <= {} THEN 'Medio y Disminuyendo'
-        WHEN dias_compra > {} AND dias_compra <= {} AND cambio_venta > {} AND cambio_venta <= {} THEN 'Medio y Estable'
-        WHEN dias_compra > {} AND dias_compra <= {} AND cambio_venta > {} THEN 'Medio y Aumentando'
-        WHEN dias_compra > {} AND cambio_venta <= {} THEN 'Alto y Disminuyendo'
-        WHEN dias_compra > {} AND cambio_venta > {} AND cambio_venta <= {} THEN 'Alto y Estable'
-        ELSE 'Alto y Aumentando'
-    END AS segmento
-""".format(corto, negativo, corto, negativo, neutro, corto, neutro,
-           corto, largo, negativo, largo, negativo, neutro, largo, neutro,
-           largo, negativo, largo, negativo, neutro))
-
-# Aplicar la segmentación
-segmentacion = tendencia_ventas.withColumn("segmento", segmento_expr)
+tendencia_ventas = df.groupBy("user_id") \
+    .agg(datediff(spark_max("order_date"), spark_min("order_date")).alias("dias_compra"))
 
 
 
-
-segmentacion.write \
+tendencia_ventas.write \
 .format("bigquery") \
 .option("temporaryGcsBucket",bucket) \
 .save("datamart_ventas.bi_segmentacion")
